e8c493b28a5531d6994b5d8ce2d99113db5643a8
Levent <levent1132@engineer.com>
2018-10-08 15:33:36 +0300

    Add files via upload

---
+++ inc/fonk.php
@@ -0,0 +1,9 @@
+<?php
+$baglanti = new mysqli('localhost', 'root', 'qweasdzxc', 'form');
+if (mysqli_connect_error()) 
+{
+    echo mysqli_connect_error();
+    exit; 
+}
+$baglanti->set_charset("utf8"); 
+?>
